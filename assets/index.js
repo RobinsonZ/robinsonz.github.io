ec5d8dfa431286b6794129307f0b6670deb24b51
Zachary Robinson <zrobins2@swarthmore.edu>
2022-08-09 23:34:55 -0500

    Draft new page design

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,7 @@
 import { tsParticles } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 
-
-loadFull(tsParticles); 
+loadFull(tsParticles);
 
 tsParticles
   .loadJSON("ts-particles", "/dist/static/particles.json")
@@ -13,19 +12,23 @@ tsParticles
     console.error(error);
   });
 
-
 // animation stuff
 document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    var replacers = document.querySelectorAll("[data-replace]");
-    for (var i = 0; i < replacers.length; i++) {
-      let replaceClasses = JSON.parse(
-        replacers[i].dataset.replace.replace(/'/g, '"')
-      );
-      Object.keys(replaceClasses).forEach(function (key) {
-        replacers[i].classList.remove(key);
-        replacers[i].classList.add(replaceClasses[key]);
-      });
-    }
-  }, 1000);
+  var doReplace = function (attribute) {
+    return function () {
+      var replacers = document.querySelectorAll("[data-" + attribute + "]");
+      for (var i = 0; i < replacers.length; i++) {
+        let replaceClasses = JSON.parse(
+          replacers[i].dataset[attribute].replace(/'/g, '"')
+        );
+        Object.keys(replaceClasses).forEach(function (key) {
+          replacers[i].classList.remove(key);
+          replacers[i].classList.add(replaceClasses[key]);
+        });
+      }
+    };
+  };
+
+  setTimeout(doReplace("replace-1s"), 1000);
+  setTimeout(doReplace("replace-2s"), 2000);
 });
